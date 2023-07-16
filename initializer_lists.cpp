70617f6138b51c39f76a0e8494e812f4bbedb4cc
maxwynne <76392789+maxwynne@users.noreply.github.com>
2023-07-16 21:27:19 +0100

    Create initializer_lists.cpp

---
+++ initializer_lists.cpp
@@ -0,0 +1,9 @@
+double d1 = 2.3;
+double d2 {2.3};
+complex<double> z = 1; // complex number with double-precision floating-point scalars
+complex<double> z2 {d1, d2};
+complex<double> z3 = {1,2}; // the = is optional with { ... }
+
+vector<int> v {1,2,3,4,5,6}; // a vector of ints
+
+int i1 = 7.8'
