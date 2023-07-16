c6d56b2d509fa1882dc64874e01b50d20eed6da6
maxwynne <76392789+maxwynne@users.noreply.github.com>
2023-07-16 21:11:45 +0100

    Create function_declarations.cpp

---
+++ function_declarations.cpp
@@ -0,0 +1,16 @@
+Elem* next_elem(); // return a pointer to Elem (an Elem*)
+void exit(int); // int argument returning nothing
+double sqrt(double); // double argument, return a double
+
+double s2 = sqrt(2);
+double s3 = sqrt("three");
+
+double get(const vector<double>& vec, int index);
+char& String::operator[](int index);
+
+void user()
+{
+    print(42);
+    print(9.65);
+    print("D is for Digital");
+}
