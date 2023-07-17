10761dde2231f877a627cb24cec621afba149ba5
maxwynne <76392789+maxwynne@users.noreply.github.com>
2023-07-17 20:21:31 +0100

    Update new_objects.cpp

--- new_objects.cpp
+++ new_objects.cpp
@@ -1,14 +1,14 @@
-vector<int> vec;
+vector<int> vec; // vec = global
 
 struct Record {
-    string name;
+    string name; // name = member (a string member)
     // ...
 };
 
-void fct(int arg)
+void fct(int arg) // fct = global function, arg = local integer argument
 
 {
-    string motto {"Who dares wins"};
-    auto p = new Record{"Hume"};
+    string motto {"Who dares wins"}; // motto = local
+    auto p = new Record{"Hume"}; // p points to unnamed Record (created by new)
     // ...
 }
