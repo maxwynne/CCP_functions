a759f9ca133cc5e628d5de0a955d4fcd8a71e50e
maxwynne <76392789+maxwynne@users.noreply.github.com>
2023-07-17 20:20:22 +0100

    Create new_objects.cpp

---
+++ new_objects.cpp
@@ -0,0 +1,14 @@
+vector<int> vec;
+
+struct Record {
+    string name;
+    // ...
+};
+
+void fct(int arg)
+
+{
+    string motto {"Who dares wins"};
+    auto p = new Record{"Hume"};
+    // ...
+}
