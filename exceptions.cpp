fee0683ecad2fd0b5d3c2340eb2116e81322a142
maxwynne <76392789+maxwynne@users.noreply.github.com>
2024-04-21 21:44:10 +0100

    Update exceptions.cpp

--- exceptions.cpp
+++ exceptions.cpp
@@ -1,3 +1,5 @@
+// make out-of-range response if there is an attempt to make out-of-range access by a user, e.g., a clear, out-of-range exception 
+
 double& Vector::operator[](int i)
 {
     if(i<0 || size()<=i)
